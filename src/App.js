e3d680b849f7e9764dfd90998b3847f5fb988935
VikaBilyk <124256530+Observersss@users.noreply.github.com>
2024-07-02 15:06:47 +0300

    а

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useCallback, useEffect, useMemo, useState} from "react";
+import './style/App.css'
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./components/hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./components/hooks/useFetching";
+import {getPageCount} from "./utils/pages";
 function App() {
+
+    const [posts, setPosts] = useState([]);
+
+    const [filter, setFilter] = useState({sort:'', query:''});
+    const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+
+    let pagesArray = [];
+    for (let i = 0; i < totalPages; i++) {
+                pagesArray.push(i + 1)
+    }
+    // const updatePagesArray = useMemo(() => {
+    //     for (let i = 0; i < totalPages; i++) {
+    //         pagesArray.push(i + 1)
+    //     }
+    //     return pagesArray;
+    // }, [totalPages]);
+
+
+    const [fetchPosts,isPostsLoading,postError] = useFetching(async ()=>{
+        const response = await PostService.getAll(limit,page);
+        setPosts(response.data);
+
+        const totalCount = response.headers['x-total-count'];
+        setTotalPages(getPageCount(totalCount,limit));
+    })
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost]);
+        setModal(false);
+    };
+
+
+    const removePost = (post ) => {
+        setPosts(posts.filter(p => p.id !== post.id));
+    };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MyButton onClick={fetchPosts}>Get posts</MyButton>
+        <MyButton style={{marginTop: '30px '}}  onClick={() => setModal(true)}>
+            Create user
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost}/>
+        </MyModal>
+
+        <hr style={{margin: "15px 0"}}/>
+
+        <PostFilter
+            filter={filter}
+            setFilter={setFilter}
+        />
+
+        {postError && <h1>Sorry what`s wrong... ${postError}</h1>}
+
+        {isPostsLoading
+            ? <div style={{ display:'flex',justifyContent: 'center', marginTop: 50}}><Loader/></div>
+            : <PostList
+                remove={removePost}
+                posts={sortedAndSearchedPosts}
+                title={'Post list'}
+            />
+        }
     </div>
   );
 }
